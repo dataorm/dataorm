52e20b4009d9f2dd547beefdb11e7fcc15762190
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-13 21:47:23 +0530

    added ioc branch

--- example/index.tsx
+++ example/index.tsx
@@ -3,9 +3,13 @@ import 'react-app-polyfill/ie11';
 import * as ReactDOM from 'react-dom';
 import App from './src/App';
 import { OrmProvider, StoreConfig } from 'dataorm';
+import { User } from './src/models/User';
 
 const config = new StoreConfig();
-const store = config.init();
+const store = config
+  .config({ name: 'hello' })
+  .add(User)
+  .init();
 
 ReactDOM.render(
   <OrmProvider store={store}>
