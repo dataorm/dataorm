16555fc887d1eaeea5ed2f2c531a5fbff5752264
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-15 07:25:46 +0530

    added ioc branch

--- src/IoC/container.ts
+++ src/IoC/container.ts
@@ -3,6 +3,9 @@ import { Subject } from '../Observer/Subject';
 import { Store } from '../Store/Store';
 import { StoreConfig } from '../Store/StoreConfig';
 import { TYPES } from './types';
+import { QueryBuilder } from '../Store/QueryBuilder';
+import { Query } from '../Store/Query';
+import { Mutation } from '../Store/Mutation';
 
 var container = new Container();
 
@@ -19,6 +22,21 @@ container
 container
   .bind<StoreConfig>(TYPES.StoreConfig)
   .to(StoreConfig)
+  .inSingletonScope();
+
+container
+  .bind<Query>(TYPES.Query)
+  .to(Query)
+  .inSingletonScope();
+
+container
+  .bind<Mutation>(TYPES.Mutation)
+  .to(Mutation)
+  .inSingletonScope();
+
+container
+  .bind<QueryBuilder>(TYPES.QueryBuilder)
+  .to(QueryBuilder)
   .inSingletonScope();
 
 export { container };
--- src/IoC/types.ts
+++ src/IoC/types.ts
@@ -2,4 +2,7 @@ export const TYPES = {
   Store: 'Store',
   StoreConfig: 'StoreConfig',
   Subject: 'Subject',
+  Query: 'Query',
+  Mutation: 'Mutation',
+  QueryBuilder: 'QueryBuilder',
 };
